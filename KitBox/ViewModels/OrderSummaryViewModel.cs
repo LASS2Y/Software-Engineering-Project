98e494df02b92d18baa9efb24c4cca3d4f8791ae
Mike-oss-star <23390@ecam.be>
2026-03-31 09:45:15 +0200

    slogan

--- KitBox/ViewModels/OrderSummaryViewModel.cs
+++ KitBox/ViewModels/OrderSummaryViewModel.cs
@@ -116,5 +116,5 @@ public partial class OrderSummaryViewModel : ViewModelBase
 
     [RelayCommand]
     public void ReturnToStart()
-        => _main.NavigateTo(new StartPageViewModel(_main));
+        => _main.NavigateTo(new WelcomePageViewModel(_main));
 }
